f35ed941bb04b4db4409b44462310fe6eabba1a4
Priyankadoke12345 <priyankaadoke@gmail.com>
2021-09-18 09:00:46 +0530

    Initial commit

---
+++ src/OverideMain.java
@@ -0,0 +1,9 @@
+package PolymorphismExample;
+
+public class OverideMain {
+    public static void main(String[] args) {
+        Parent ob = new Child();
+        ob.show();
+    }
+}
+
---
+++ src/Parent.java
@@ -0,0 +1,10 @@
+
+package PolymorphismExample;
+
+public class Parent {
+    void show(){
+        System.out.println("parent class method");
+    }
+}
+
+
